c2eaecd14fec8d176425024740e9bff775b285a0
Adam Wasiljew <awasiljew@gmail.com>
2015-09-29 21:41:02 +0200

    serial port factory

--- SerialPortDriver/src/main/java/pl/awasiljew/spd/controller/factory/SerialPortControllerFactory.java
+++ SerialPortDriver/src/main/java/pl/awasiljew/spd/controller/factory/SerialPortControllerFactory.java
@@ -1,10 +1,27 @@
 package pl.awasiljew.spd.controller.factory;
 
+import pl.awasiljew.spd.controller.SerialPortController;
+import pl.awasiljew.spd.controller.impl.SerialPortControllerImpl;
+import pl.awasiljew.spd.data.buffers.ByteFramesBuffer;
+import pl.awasiljew.spd.data.buffers.ByteFramesBufferConfig;
+import pl.awasiljew.spd.data.protocol.ByteDataParser;
+import pl.awasiljew.spd.port.factory.SerialPortInstanceFactory;
+import pl.awasiljew.spd.port.settings.SerialPortSettings;
+
 /**
  * @author Adam Wasiljew
  */
 public class SerialPortControllerFactory {
 
-    // TODO
+    public SerialPortController createInstance(ByteDataParser dataParser, SerialPortInstanceFactory serialPortInstanceFactory, SerialPortSettings serialPortSettings) {
+        ByteFramesBufferConfig config = new ByteFramesBufferConfig();
+        ByteFramesBuffer framesBuffer = new ByteFramesBuffer(dataParser, config);
+        return new SerialPortControllerImpl(
+                serialPortSettings,
+                serialPortInstanceFactory,
+                framesBuffer,
+                framesBuffer
+        );
+    }
 
 }
